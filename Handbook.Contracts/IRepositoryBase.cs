ff8b6ac46a9f4f49b1c64f79bb48177861f2e1b3
AlexanderKarcheuski <karchevskji@gmail.com>
2022-03-21 14:59:45 +0300

    Created contracts and repository

---
+++ Handbook.Contracts/IRepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handbook.Contracts
+{
+    public interface IRepositoryBase<T>
+    {
+        void Create(T entity);
+        void Delete(T entity);
+        IQueryable<T> FindAll(bool trackChanges);
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges);
+    }
+}
---
+++ Handbook.Repository/RepositoryBase.cs
@@ -0,0 +1,37 @@
+﻿using Handbook.Contracts;
+using Handbook.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Handbook.Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T: class
+    {
+        protected HandbookContext HandbookContext { get; set; }
+
+        public RepositoryBase(HandbookContext handbookContext)
+        {
+            HandbookContext = handbookContext;
+        }
+
+        public void Create(T entity) => HandbookContext.Set<T>().Add(entity);
+
+        public void Delete(T entity) => HandbookContext.Set<T>().Remove(entity);
+
+        public IQueryable<T> FindAll(bool trackChanges) =>
+            !trackChanges ?
+            HandbookContext.Set<T>()
+            .AsNoTracking() :
+            HandbookContext.Set<T>();
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges) =>
+            !trackChanges ?
+            HandbookContext.Set<T>()
+            .Where(expression)
+            .AsNoTracking() :
+            HandbookContext.Set<T>()
+            .Where(expression);
+    }
+}
